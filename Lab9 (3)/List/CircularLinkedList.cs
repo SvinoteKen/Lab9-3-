d162296c136102371d7f99fcb889fe525d4c9999
SvinoteKen <stastad2013@gmail.com>
2020-05-21 12:46:30 +0300

    Add project files.

---
+++ Lab9 (3)/List/CircularLinkedList.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9__3_.List
+{
+    public class CircularLinkedList<T> : IEnumerable<T>
+    {
+        Node<T> head;
+        Node<T> tail;
+        int count;
+        public void Add(T data)
+        {
+            var node = new Node<T>(data);
+            if (head == null)
+            {
+                head = node;
+                tail = node;
+                tail.Next = head;
+            }
+            else
+            {
+                node.Next = head;
+                tail.Next = node;
+                tail = node;
+            }
+            count++;
+        }
+        public bool Remove(T data)
+        {
+            Node<T> current = head;
+            Node<T> previous = null;
+
+            if (IsEmpty) return false;
+
+            do
+            {
+                if (current.Data.Equals(data))
+                {
+                    
+                    if (previous != null)
+                    {
+                        previous.Next = current.Next;
+                        if (current == tail)
+                            tail = previous;
+                    }
+                    else 
+                    {
+                        if (count == 1)
+                        {
+                            head = tail = null;
+                        }
+                        else
+                        {
+                            head = current.Next;
+                            tail.Next = current.Next;
+                        }
+                    }
+                    count--;
+                    return true;
+                }
+
+                previous = current;
+                current = current.Next;
+            } while (current != head);
+
+            return false;
+        }
+
+        public int Count { get { return count; } }
+        public bool IsEmpty { get { return count == 0; } }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public bool Contains(T data)
+        {
+            Node<T> current = head;
+            if (current == null) return false;
+            do
+            {
+                if (current.Data.Equals(data))
+                    return true;
+                current = current.Next;
+            }
+            while (current != head);
+            return false;
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T> current = head;
+            do
+            {
+                if (current != null)
+                {
+                    yield return current.Data;
+                    current = current.Next;
+                }
+            }
+            while (current != head);
+        }
+        public void Swap(T data)
+        {
+            Node<T> current = FindElement(data);
+            tail = current;
+            head = current.Next;
+            head = tail;
+        }
+        public Node<T> FindElement(T data)
+        {
+            Node<T> current = head;
+            if (current == null) return null;
+            do
+            {
+                if (current.Data.Equals(data))
+                    return current;
+                current = current.Next;
+            }
+
+            while (current != head);
+            return null;
+        }
+    }
+}
---
+++ Lab9 (3)/Program.cs
@@ -0,0 +1,64 @@
+﻿using Lab9__3_.List;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9__3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CircularLinkedList<string> members = new CircularLinkedList<string>();
+            string path = @"C:\Users\MSI\source\repos\Lab9 (3)\Lab9 (3)\List\Имена.txt";
+            using (var fileRead = new StreamReader(path))
+            {
+                while (fileRead.Peek() > -1)
+                {
+                    members.Add(fileRead.ReadLine());
+                }
+            }
+            foreach (string member in members)
+            {
+                Console.Write(member + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Введите человека с кого начать считалку?");
+            string name = "";
+            while (!members.Contains(name))
+            {
+                name = Console.ReadLine();
+                if (!members.Contains(name))
+                {
+                    Console.WriteLine("Имя не найдено введите из списка:");
+                    foreach (string member in members)
+                    {
+                        Console.Write(member + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine("Введите считалочку");
+            string text = Console.ReadLine();
+            string[] word = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            members.Swap(name);
+            Сhoice(members, word.Length-1);
+
+        }
+        public static void Сhoice(CircularLinkedList<string> members, int Length)
+        {
+            int count = 0;
+            foreach (string name in members)
+            {
+                if (count == Length)
+                {
+                    Console.WriteLine(name);
+                }
+                count++;
+            }
+        }
+    }
+}
